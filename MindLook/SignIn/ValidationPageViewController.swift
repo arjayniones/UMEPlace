f131137d9e36352f2be276a7cd4d9f2eb4eb82e1
Arjay Niones <arjayniones@2405-9800-ba10.44.pool1.tls1b-bcr02.myaisfibre.com>
2018-03-29 10:05:59 +0700

    Sign in, sign up, forgot password, validation code finished

--- MindLook/SignIn/ValidationPageViewController.swift
+++ MindLook/SignIn/ValidationPageViewController.swift
@@ -47,27 +47,81 @@ class ValidationPageViewController: UIViewController {
         
         if self.passedUserActCode == self.txtValidationCode.text {
             
-            let alert = UIAlertController(title: "Success!!!", message: "Verification Code matched.", preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.default, handler:   { action in
+            
+            let myUrl = URL(string: "http://103.86.48.128/Signup/checkvalidatecode?");
+            
+            var request = URLRequest(url:myUrl!)
+            
+            request.httpMethod = "POST"// Compose a query string
+            
+            let postString = "userid=\(self.passedUserID)&activatecode=\(self.txtValidationCode.text!)";
+            
+            request.httpBody = postString.data(using: String.Encoding.utf8);
+            
+            let task = URLSession.shared.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
                 
-                // do something like...
-                self.gotoMain()
+                if error != nil
+                {
+                    print("error=\(error)")
+                    return
+                }
                 
-                }))
+                // You can print out response object
+                print("response = \(response)")
+                
+                //Let's convert response sent from a server side script to a NSDictionary object:
+                do {
+                    
+                    
+                    
+                    
+                    OperationQueue.main.addOperation({
+                        
+                        let result = String(data: data!, encoding: .utf8)
+                        
+                        print("This is the JSON Data: \(result!)")
+                        if result == "1" {
+                            
+                            
+                            let alert = UIAlertController(title: "Success!!!", message: "Verification Code matched.", preferredStyle: UIAlertControllerStyle.alert)
+                            alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.default, handler:   { action in
+                                
+                                // do something like...
+                                self.gotoMain()
+                                
+                            }))
+                            
+                            self.present(alert, animated: true, completion: nil)
+                            
+                            
+                        }else if result == "-1"{
+                            
+                            
+                            
+                            let alert = UIAlertController(title: "Code did not match", message: "Please check email and try again.", preferredStyle: UIAlertControllerStyle.alert)
+                            alert.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                            
+                            
+                        }
+                        else {
+                            
+                            
+                        }
+                        
+                    })
+                  
+                    
+                } catch {
+                    print(error)
+                    
+                    
+                }
+            }
+            task.resume()
             
-            self.present(alert, animated: true, completion: nil)
-            
-           
-            
-           
-            
-        }else {
-            let alert = UIAlertController(title: "Code did not match", message: "Please check email and try again.", preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
             
         }
-        
         
     }
     
